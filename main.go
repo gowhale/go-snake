95f438b5b2f8332e5f759b4ea2b7b83be1617489
gowhale <gabewhale@gmail.com>
2022-11-20 00:42:18 +0000

    feat(direction): add direction and move accordingly

--- main.go
+++ main.go
@@ -20,19 +20,26 @@ func main() {
 		ColPins: make([]int, size),
 	})
 
-	snk := snake.NewSnake([]int{4, 2}, [][]int{{5, 2}, {6, 2}, {7, 2}})
+	snk := snake.NewSnake([]int{4, 2}, [][]int{{5, 2}, {6, 2}, {7, 2}}, size, size)
 
 	cvs := canvas.NewCanvas(size, size, &snk)
 	cvs.GetMatrix()
 	scrn.DisplayMatrix(cvs.GetMatrix(), time.Second)
+
+	for j := 0; j < 100; j++ {
+		snk.NextMove()
+		scrn.DisplayMatrix(cvs.GetMatrix(), time.Second)
+	}
+	
+	// snk.ChangeDirection(snake.East)
+	snk.NextMove()
+	scrn.DisplayMatrix(cvs.GetMatrix(), time.Second)
+	// snk.ChangeDirection(snake.South)
 	snk.NextMove()
 	scrn.DisplayMatrix(cvs.GetMatrix(), time.Second)
 	snk.NextMove()
 	scrn.DisplayMatrix(cvs.GetMatrix(), time.Second)
-	snk.NextMove()
-	scrn.DisplayMatrix(cvs.GetMatrix(), time.Second)
-	snk.NextMove()
-	scrn.DisplayMatrix(cvs.GetMatrix(), time.Second)
+	// snk.ChangeDirection(snake.West)
 	snk.NextMove()
 	scrn.DisplayMatrix(cvs.GetMatrix(), time.Second)
 }
--- pkg/snake/snake.go
+++ pkg/snake/snake.go
@@ -10,16 +10,19 @@ const (
 )
 
 type Snake struct {
-	direction string
-	headCord  []int
-	tailCord  [][]int
+	direction      string
+	xLimit, yLimit int
+	headCord       []int
+	tailCord       [][]int
 }
 
-func NewSnake(headCord []int, tailCord [][]int) Snake {
+func NewSnake(headCord []int, tailCord [][]int, xLimit, yLimit int) Snake {
 	return Snake{
 		direction: North,
 		headCord:  headCord,
 		tailCord:  tailCord,
+		xLimit:    xLimit,
+		yLimit:    yLimit,
 	}
 }
 
@@ -27,18 +30,73 @@ func (s *Snake) Body() [][]int {
 	return append([][]int{s.headCord}, s.tailCord...)
 }
 
+func (s *Snake) UpdatePositions(x, y int) {
+	s.tailCord = s.tailCord[:len(s.tailCord)-1]
+	s.tailCord = append([][]int{s.headCord}, s.tailCord...)
+	headX, headY := s.headCord[0]+x, s.headCord[1]+y
+	if headX < 0 {
+		log.Println("SETTING X LIM")
+		headX = s.xLimit - 1
+	}
+	if headY < 0 {
+		log.Println("SETTING Y LIM")
+		headY = s.yLimit - 1
+		log.Println(s.yLimit - 1)
+		log.Println(headY)
+		s.headCord[1] = headY
+	}
+	if headX > s.xLimit-1 {
+		headX = 0
+	}
+	if headY > s.yLimit-1 {
+		headY = 0
+	}
+	log.Println(headX, headY)
+	s.headCord = []int{headX, headY}
+}
+
 func (s *Snake) NextMove() {
 	switch s.direction {
 	case North:
 		log.Println("Going north!")
-		s.tailCord = s.tailCord[:len(s.tailCord)-1]
-		s.tailCord = append([][]int{s.headCord}, s.tailCord...)
-		s.headCord = []int{s.headCord[0], s.headCord[1] - 1}
+		s.UpdatePositions(0, -1)
 	case South:
 		log.Println("Going south!")
+		s.UpdatePositions(0, 1)
 	case East:
 		log.Println("Going east!")
+		s.UpdatePositions(1, 0)
 	case West:
 		log.Println("Going west!")
+		s.UpdatePositions(-1, 0)
+	}
+}
+
+var possibleDirections = map[string]map[string]bool{
+	North: map[string]bool{
+		North: true,
+		East:  true,
+		West:  true,
+	},
+	South: map[string]bool{
+		South: true,
+		East:  true,
+		West:  true,
+	},
+	East: map[string]bool{
+		North: true,
+		East:  true,
+		South: true,
+	},
+	West: map[string]bool{
+		North: true,
+		South: true,
+		West:  true,
+	},
+}
+
+func (s *Snake) ChangeDirection(direction string) {
+	if _, ok := possibleDirections[s.direction][direction]; ok {
+		s.direction = direction
 	}
 }
